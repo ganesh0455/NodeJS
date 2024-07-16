012adbcec28758a84dca757044965a8b0e72ef57
Ganesh V <ganesh.c@veltris.com>
2024-07-16 22:30:32 +0530

    Redirecting Requests

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 function requestListener(req, res) {
     const url = req.url;
+    const method = req.method;
 
     if (url === '/') {
         res.write('<html>');
         res.write('<head>');
         res.write('<title>My first Page</title>');
         res.write('</head>')
-        res.write('<body><form action="message" method="POST" ><input type="text" name="enteredMessage" /><button type="submit">Submit</button></form></body>');
+        res.write('<body><form action="/message" method="POST" ><input type="text" name="enteredMessage" /><button type="submit">Submit</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.text', "Dummy");
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
